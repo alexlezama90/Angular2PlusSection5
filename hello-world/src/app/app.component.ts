bdbfc810dcd300ad2b32ec9df68bca8101aee6c9
Alejandro Lezama <alexlezama90@gmail.com>
2019-06-05 15:24:42 -0500

    section 5 assignment finished

--- hello-world/src/app/app.component.ts
+++ hello-world/src/app/app.component.ts
@@ -1,37 +1,23 @@
 import { Component } from '@angular/core';
 import { FavoriteChangedEventArgs } from './favorite/favorite.component';
+// import { ITweetData } from './like/like.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  styles: [`
-  .glyphicon {
-    color: green;
-  }
-  `]
 })
 export class AppComponent {
-  post = {
-    title: 'Title',
-    isFavorite: true
+  tweet = {
+    body: 'AppComponent Text',
+    isLiked: true,
+    likesCount: 10
   }
 
-  // onFavoriteChange(isFavorite){
-  //   console.log("Favorite changed: ", isFavorite);
+  // onLikeChanged(tweetData: ITweetData){
+  //   console.log("parent likes = ", this.tweet.likesCount +  "child likes = ", tweetData.likesCount);
+  //   // console.log(this.tweet.body);
   // }
-
-  // onFavoriteChange(eventArgs){
-  //   console.log("Favorite changed: ", eventArgs);
-  // }
-
-  // onFavoriteChange(eventArgs: { newValue: boolean}){ //We can use inline annotations to get Intellisense
-  //   console.log("Favorite changed: ", eventArgs.newValue);
-  // }
-
-  onFavoriteChange(eventArgs: FavoriteChangedEventArgs) { //We can define interfaces to make it even more understandable
-    console.log("Favorite changed: ", eventArgs); //eventArgs.value
-  }
 }
 
 
--- hello-world/src/app/like/like.component.ts
+++ hello-world/src/app/like/like.component.ts
@@ -1,11 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-like',
+  selector: 'like',
   templateUrl: './like.component.html',
   styleUrls: ['./like.component.css']
 })
 export class LikeComponent implements OnInit {
+  @Input('likesCount') likesCount: number;
+  @Input('isActive') isActive: boolean;
+
+  // @Output() click = new EventEmitter();
+
+  onClick(){
+    this.likesCount += (this.isActive) ? -1 : 1;
+    this.isActive = !this.isActive;
+    // let tweetData = new TweetData('...', this.likesCount, this.isActive);
+
+    // this.click.emit();
+  }
 
   constructor() { }
 
@@ -13,3 +25,15 @@ export class LikeComponent implements OnInit {
   }
 
 }
+
+// export interface ITweetData{
+//   body: string;
+//   likesCount: number;
+//   isLiked: boolean;
+// }
+
+// export class TweetData implements ITweetData{
+
+//   constructor(public body: string, public likesCount: number, public isLiked: boolean) {
+//   }
+// }
